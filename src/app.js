c31e50debc7f0b996714452b0184b27fd8c482fb
jorgeyebenes <159541179+jorgeyebenes@users.noreply.github.com>
2024-04-17 04:00:09 +0000

    GENERADOR DE SALUDOS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#RESPUESTA").innerHTML = generadorSALUDOS();
+  });
+  console.log("SALUDOS DESDE ESPAÑA");
+  this;
+};
+
+let generadorSALUDOS = () => {
+  let mellamo = [
+    "Me llamo",
+    "My name is",
+    "Je m'appelle",
+    "Ich HeiBe",
+    "Меня зовут",
+    "اسمي هو"
+  ];
+  let nombre = ["JORGE", "MICHAEL", "PIERRE", "LUDWIG", "VLADIMIR", "HASSAN"];
+  let bienvenida = [
+    "bienvenidos a",
+    "welcome to",
+    "bienvenue a la",
+    "willkommen zu",
+    "Добро пожаловать в",
+    "مرحبا بك في"
+  ];
+  let pais = ["ESPAÑA", "USA", "FRANCE", "DEUTSCHLAND", "RUSSIA", "PALESTINE"];
+
+  let mellamoINDEX = Math.floor(Math.random() * mellamo.length);
+  let nombreINDEX = Math.floor(Math.random() * nombre.length);
+  let bienvenidaINDEX = Math.floor(Math.random() * bienvenida.length);
+  let paisINDEX = Math.floor(Math.random() * pais.length);
+
+  return (
+    mellamo[mellamoINDEX] +
+    " " +
+    nombre[nombreINDEX] +
+    " " +
+    bienvenida[bienvenidaINDEX] +
+    " " +
+    pais[paisINDEX]
+  );
 };
